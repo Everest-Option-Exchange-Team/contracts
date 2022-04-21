7c8030f6a90e01b59dee8f369fc1e84946006048
Linus Weigand <linusweigand@protonmail.com>
2022-04-21 10:53:26 +0200

    only whole options allowed

--- contracts/ERC20OptionToken.sol
+++ contracts/ERC20OptionToken.sol
@@ -8,4 +8,11 @@ contract OptionToken is ERC20 {
     constructor(uint256 initialSupply, string memory tickerSymbol, string memory tokenName) ERC20(tickerSymbol, tokenName) {
         _mint(msg.sender, initialSupply);
     }
+
+    /*
+     *  @dev only whole option contracts allowed, no fractional options, might change in the future, for now to simplify
+     */
+    function decimals() public view virtual override returns (uint8) {
+        return 0;
+    }
 }
