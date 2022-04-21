de7c1d287f8ca7f288355e08cfb0ba325e256d78
Linus Weigand <linusweigand@protonmail.com>
2022-04-21 10:45:56 +0200

    ERC20 Token contract constructor

--- contracts/ERC20Minter.sol
+++ contracts/ERC20Minter.sol
@@ -1,1 +1,11 @@
-import “https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/presets/ERC20PresetMinterPauser.sol”;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract OptionToken is ERC20 {
+
+    constructor(uint256 initialSupply) ERC20("Option", "Option") {
+        _mint(msg.sender, initialSupply);
+    }
+}
