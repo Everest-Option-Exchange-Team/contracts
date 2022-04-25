7fdf75b6fc154ae61195358d0ed6864fba25c172
leovct <l009.vincent@gmail.com>
2022-04-25 13:31:55 +0200

    add kovan custom node operator contract

--- contracts/kovan/KovanStockAPIConsumer.sol
+++ contracts/kovan/KovanStockAPIConsumer.sol
@@ -5,13 +5,14 @@ import "../chainlink/StockAPIConsumer.sol";
 
 /**
  * @title Contract that consumes the Alpha Vantage stock price API on the Kovan testnet.
+ * @dev It uses Chainlink node operator.
  * @author The Everest team.
  */
 contract KovanStockAPIConsumer is StockAPIConsumer {
     /**
      * @notice Initialise the contract.
      * @param _apiKey the alpha vantage api key.
-     * @dev Kovan testnet Alpha Vantage stock price API parameters:
+     * @dev Kovan testnet, Chainlink Operator, Alpha Vantage stock price API parameters:
      * Oracle address: 0xc57B33452b4F7BB189bB5AfaE9cc4aBa1f7a4FD8.
      * Job ID: d5270d1c311941d0b08bead21fea7747.
      * URL: https://docs.chain.link/docs/decentralized-oracles-ethereum-mainnet/
