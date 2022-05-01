2c630b75db5d5dfe7f6ffe36deefe0e9e76f7c19
leovct <l009.vincent@gmail.com>
2022-05-01 18:26:53 +0200

    implement tests

--- test/api-consumer.js
+++ test/api-consumer.js
@@ -1,94 +1,100 @@
 const hre = require("hardhat");
+const { utils } = require("ethers");
 const { network } = require("hardhat");
+const { expect } = require("chai");
 
 // Check if we're deploying to a local blockchain.
 // If yes, run the tests.
 const LOCAL_CHAIN_ID = 31337;
 if (network.config.chainId === LOCAL_CHAIN_ID) {
-    describe("Price Consumer smart contract tests", () => {
-        let stockAPIConsumerContract, owner, user, oracle, temp;
+	describe("Price Consumer smart contract tests", () => {
+		let stockAPIConsumerContract, mockOracleContract, owner, user, oracle, temp;
 
-        beforeEach(async () => {
-            // Define an owner and a user.
-            const accounts = await hre.ethers.getSigners();
-            owner = accounts[0];
-            user = accounts[1];
-            oracle = accounts[2];
-            temp = accounts[3];
+		beforeEach(async () => {
+			// Define an owner and a user.
+			const accounts = await hre.ethers.getSigners();
+			owner = accounts[0];
+			user = accounts[1];
+			oracle = accounts[2];
+			temp = accounts[3];
 
-            // Deploy a mock link token contract.
-            const linkTokenContractFactory = await hre.ethers.getContractFactory("LinkToken");
-            const linkTokenContract = await linkTokenContractFactory.deploy();
-            await linkTokenContract.deployed();
-            console.log("LINK token contract deployed to:", linkTokenContract.address);
+			// Deploy all the mock contracts.
+			// 1. Deploy a mock link token contract.
+			const linkTokenContractFactory = await hre.ethers.getContractFactory("LinkToken");
+			const linkTokenContract = await linkTokenContractFactory.deploy();
+			await linkTokenContract.deployed();
+			console.log("LINK token contract deployed to:", linkTokenContract.address);
 
-            // Deploy the stock api consumer contract.
-            const stockAPIConsumerContractFactory = await hre.ethers.getContractFactory("StockAPIConsumer");
-            stockAPIConsumerContract = await stockAPIConsumerContractFactory.deploy(linkTokenContract.address, oracle.address, ethers.constants.HashZero, "apikey");
-            await stockAPIConsumerContract.deployed();
-            console.log("StockAPIConsumer contract deployed to:", stockAPIConsumerContract.address);
+			// 2. Deploy a mock oracle contract.
+			const mockOracleContractFactory = await hre.ethers.getContractFactory("MockOracle");
+			mockOracleContract = await mockOracleContractFactory.deploy(linkTokenContract.address);
+			await mockOracleContract.deployed();
+			console.log("Mock oracle contract deployed to:", mockOracleContract.address);
 
-            /*
-            // URL: https://github.com/smartcontractkit/hardhat-starter-kit/blob/main/test/unit/APIConsumer_unit_test.js
+			// Deploy the stock api consumer contract.
+			const stockAPIConsumerContractFactory = await hre.ethers.getContractFactory("StockAPIConsumer");
+			stockAPIConsumerContract = await stockAPIConsumerContractFactory.deploy(linkTokenContract.address, oracle.address, ethers.constants.HashZero, "apikey");
+			await stockAPIConsumerContract.deployed();
+			console.log("StockAPIConsumer contract deployed to:", stockAPIConsumerContract.address);
 
-            // Deploy the mock oracle contract.
-            const mockOracleContractFactory = await hre.ethers.getContractFactory("MockOracle");
-            const mockOracleContract = await mockOracleContractFactory.deploy();
-            await mockOracleContract.deployed();
-            console.log("Mock oracle contract deployed to:", mockOracleContract.address);
+			// Fund the stock api consumer contract with some LINK tokens.
+			await linkTokenContract.transfer(stockAPIConsumerContract.address, utils.parseEther("1"));
+			console.log("StockAPIConsumer contract funded with 1 LINK token");
+		});
 
-            // Deploy the link token contract.
-            const linkTokenContractFactory = await hre.ethers.getContractFactory("LinkToken");
-            const linkTokenContract = await linkTokenContractFactory.deploy();
-            await linkTokenContract.deployed();
-            console.log("LINK token contract deployed to:", linkTokenContract.address);
+		/*
+		// TODO: Write the request price test function.
+		describe("RequestPrice", () => {
+			it("Should ask for the TSLA stock price", async () => {
+				// Check the TSLA price stored in the contract is null.
+				const tslaPrice = await stockAPIConsumerContract.stockToPrices("TSLA");
+				expect(tslaPrice.toNumber()).to.equal(0);
 
-            // Fund the stock api consumer contract with some LINK tokens.
-            await hre.run("fund-link", { contract: stockAPIConsumerContract.address, linkaddress: linkTokenContract.address })
-            */
-        });
+				// Request the new TSLA stock price.
+				const txn = await stockAPIConsumerContract.requestPrice("TSLA");
+				const transactionReceipt = await txn.wait();
+				const requestId = transactionReceipt.events[0].topics[1];
+				console.log("ID of the TSLA stock price request:", requestId);
+				expect(requestId).to.not.be.null
 
-        describe("RequestPrice", () => {
-            console.log("TODO");
-        });
+				// TODO: Check the result.
+				await mockOracleContract.fulfillOracleRequest(requestId, utils.formatBytes32String('TSLA'));
+			});
+		});
+		*/
 
-        describe("Fulfill", () => {
-            console.log("TODO");
-        });
+		describe("UpdateOracleAddress", () => {
+			it("Should update the oracle address", async () => {
+				const txn = await stockAPIConsumerContract.updateOracleAddress(temp.address);
+				await txn.wait();
+			});
+		});
 
+		describe("UpdateJobID", () => {
+			it("Should update the job ID", async () => {
+				const txn = await stockAPIConsumerContract.updateJobId(ethers.constants.HashZero);
+				await txn.wait();
+			});
+		});
 
-        describe("UpdateOracleAddress", () => {
-            it("Should update the oracle address", async () => {
-                const txn = await stockAPIConsumerContract.updateOracleAddress(temp.address);
-                await txn.wait();
-            });
-        });
+		describe("UpdateApiKey", () => {
+			it("Should update the api key", async () => {
+				const txn = await stockAPIConsumerContract.updateApiKey("test");
+				await txn.wait();
+			});
+		});
 
-        describe("UpdateJobID", () => {
-            it("Should update the job ID", async () => {
-                const txn = await stockAPIConsumerContract.updateJobId(ethers.constants.HashZero);
-                await txn.wait();
-            });
-        });
+		describe("Withdraw", () => {
+			it("Should withdraw money from the fund", async () => {
+				// It should succeed when the owner tries to withdraw funds (LINK tokens).
+				const txn = await stockAPIConsumerContract.connect(owner).withdraw();
+				await txn.wait();
 
-        describe("UpdateApiKey", () => {
-            it("Should update the api key", async () => {
-                const txn = await stockAPIConsumerContract.updateApiKey("test");
-                await txn.wait();
-            });
-        });
-
-        describe("Withdraw", () => {
-            it("Should withdraw money from the fund", async () => {
-                // It should succeed when the owner tries to withdraw funds (LINK tokens).
-                const txn = await stockAPIConsumerContract.connect(owner).withdraw();
-                await txn.wait();
-
-                // It should fail when any other user tries to withdraw funds.
-                await stockAPIConsumerContract.connect(user).withdraw().reverted;
-                await stockAPIConsumerContract.connect(oracle).withdraw().reverted;
-                await stockAPIConsumerContract.connect(temp).withdraw().reverted;
-            });
-        });
-    });
+				// It should fail when any other user tries to withdraw funds.
+				await stockAPIConsumerContract.connect(user).withdraw().reverted;
+				await stockAPIConsumerContract.connect(oracle).withdraw().reverted;
+				await stockAPIConsumerContract.connect(temp).withdraw().reverted;
+			});
+		});
+	});
 }
