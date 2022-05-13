a939ed397d936c0fd4a3807e558c263c5707382f
Linus Weigand <linusweigand@protonmail.com>
2022-05-13 19:31:00 +0200

    add modifier onlyAuthorizedAddresses

---
+++ contracts/Hub.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Hub is Ownable{
+    Minter minterContract;
+    Fund fundContract;
+    Storage storageContract;
+    address[] authorizedAddresses;
+
+    modifier onlyAuthorizedAddresses() {
+        bool isAuthorized = false;
+        for(uint i = 0; i < authorizedAddresses.length; i++) {
+            if(authorizedAddresses[i] == msg.sender) {
+                isAuthorized = true;
+            }
+        }
+        require(isAuthorized);
+        _;
+    }
+
+    constructor() {
+    }
+
+    function addAuthorizedAddress(address addr) public onlyOwner{
+        authorizedAddresses.push(addr);
+    }
+
+    function mintToken(address receiver, uint256 amount) public {
+        this.minterContract.mint(receiver, amount); 
+    }
+
+    function setMinterContract(address _minterAddress) public {
+        this.minterContract = new Minter(_minterAddress);
+    }
+
+    function setFundContract(address _fundAddress) public {
+        this.fundContract = new Fund(_fundAddress);
+    }
+
+    function setStorageContract(address _storageAddress) public {
+        this.storageContract = new Storage(_storageAddress);
+    }
+
+    function checkCollateralRatio() public returns (uint256) {
+         uint256 amountFunded = fundContract.getAmountFundedByAddress(msg.sender);
+         //TODO: 
+    }
+
+}
+
+interface Minter {
+    function mint(address to, uint256 amount) external;
+}
+
+interface Fund {
+    function getAmountFundedByAddress(address _addr) external view returns (uint256);
+}
+
+interface Storage {
+    //TODO: function to get price of specific stock
+}
