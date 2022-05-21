73c4dc7229aec4f9a10160b9fec13c2310a620bf
Linus Weigand <linusweigand@protonmail.com>
2022-05-21 22:08:31 +0200

    remove documentation of Minter.sol

--- contracts/Minter.sol
+++ contracts/Minter.sol
@@ -6,34 +6,18 @@ pragma solidity ^0.8.7;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-/**
- * @dev {ERC20} token, including:
- *
- *  - a minter role that allows for token minting (creation)
- */
 contract Minter is ERC20, Ownable {
-    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
 
-    /**
-     * @dev Grants `DEFAULT_ADMIN_ROLE`, `MINTER_ROLE` and `PAUSER_ROLE` to the
-     * account that deploys the contract.
-     *
-     * See {ERC20-constructor}.
-     */
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
 
-    /**
-     * @dev Creates `amount` new tokens for `to`.
-     *
-     * See {ERC20-_mint}.
-     *
-     * Requirements:
-     *
-     * - the caller must have the `MINTER_ROLE`.
-     */
+    
+   
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
+    }
+
+    function burn(address from, uint amount) public onlyOwner {
+        _burn(from, amount);
     }
 }
 
