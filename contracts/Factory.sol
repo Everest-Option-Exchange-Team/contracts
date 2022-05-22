6af4eef52f5f5e31d923832c799d9736c09dd5f7
Linus Weigand <linusweigand@protonmail.com>
2022-05-22 16:44:27 +0200

    Factory: add docs

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -11,14 +11,26 @@ import "./samples/AuthorizedAddresses.sol";
  */
 contract Factory is ERC20, AuthorizedAddresses {
 
-
+    /**
+     * @notice sets name and tickerSymbol of new ERC20 token
+     * @param name name of new ERC20 token
+     * @param symbol tickerSymbol of new ERC20 token
+     */
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
 
-   
+    /**
+     * @notice mints new tokens
+     * @param to address where tokens get minted to
+     * @param amount amount of tokens that get minted
+     */
     function mint(address to, uint256 amount) public onlyAuthorizedAddresses {
         _mint(to, amount);
     }
-
+    /**
+     * @notice burns new tokens
+     * @param from address where tokens get burned from
+     * @param amount amount of tokens that get burned
+     */
     function burn(address from, uint amount) public onlyAuthorizedAddresses {
         _burn(from, amount);
     }
