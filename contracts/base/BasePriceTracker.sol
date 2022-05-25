65694b6bf62dc108c0b48f6a1b5de3fc263cab8a
leovct <l009.vincent@gmail.com>
2022-05-25 21:02:39 +0200

    add documentation and slither comments

--- contracts/base/BasePriceTracker.sol
+++ contracts/base/BasePriceTracker.sol
@@ -116,6 +116,7 @@ contract BasePriceTracker is ChainlinkClient, KeeperCompatibleInterface {
      * @param _asset the asset name.
      * @dev The method can only be called by the hub or the owner.
      */
+     //slither-disable-next-line naming-convention
     function addAsset(string memory _asset) external {
         require(msg.sender == hubAddress || msg.sender == owner, "Only the hub and the owner can call this method");
         require(bytes(_asset).length > 0, "The asset name cannot be empty");
@@ -159,8 +160,12 @@ contract BasePriceTracker is ChainlinkClient, KeeperCompatibleInterface {
      * @notice Update the asset price using the Alpha Vantage API.
      * @param _asset the asset name.
      $ @return requestId the id of the Chainlink request.
-     * @dev The method can only be called by the keepers registry or the owner.
+     * @dev
+     * - The method can only be called by the keepers registry or the owner.
+     * - The last line of the method is detected as a reentrancy bug by Slither but it's not critical.
+     *   It would act as a double call wich would simply update twice the asset price.
      */
+     //slither-disable-next-line naming-convention
     function updateAssetPrice(string memory _asset) public returns (bytes32 requestId) {
         require(msg.sender == keepersRegistryAddress || msg.sender == owner, "Only the keepers registry and the owner can call this method");
         require(bytes(_asset).length > 0, "The asset name cannot be empty");
@@ -188,6 +193,7 @@ contract BasePriceTracker is ChainlinkClient, KeeperCompatibleInterface {
      * @param _requestId the id of the Chainlink request.
      * @param _price the new asset price.
      */
+     //slither-disable-next-line naming-convention
     function fulfill(bytes32 _requestId, uint256 _price) external recordChainlinkFulfillment(_requestId) {
         string memory asset = requestIdToAsset[_requestId];
     	assetToPrice[asset].price = _price;
@@ -198,13 +204,14 @@ contract BasePriceTracker is ChainlinkClient, KeeperCompatibleInterface {
 
     /**
      * @notice Check if the performUpkeep function should be executed.
-     * @dev See the Chainlink Keepers documentation: https://docs.chain.link/docs/chainlink-keepers/compatible-contracts/.
      * @return _ boolean that when true, will trigger the on-chain performUpkeep call.
      * @return _ bytes that will be used as input parameter when calling performUpkeep (here empty).
+     * @dev Timestamps are used for comparison which is not optimal because it can be manipulated by miners.
+     * However, this is not a critical method, it can't be exploited and it should only be called by the
+     * keepers registry or the owner.
      */
     function checkUpkeep(bytes calldata) external view override returns (bool, bytes memory) {
         require(!paused, "The contract is paused and the automatic update of prices is stopped");
-
         return((block.timestamp - lastTimeStamp) > interval, abi.encode('0x'));
     }
 
@@ -221,9 +228,7 @@ contract BasePriceTracker is ChainlinkClient, KeeperCompatibleInterface {
         require(msg.sender == keepersRegistryAddress || msg.sender == owner, "Only the keepers registry and the owner can call this method");
         
         // Re-validate the upkeep condition.
-        //slither-disable-next-line block-timestamp
         if ((block.timestamp - lastTimeStamp) > interval ) {
-            //slither-disable-next-line block-timestamp
             lastTimeStamp = block.timestamp;
 
             // Update the USDC/USD price.
@@ -272,6 +277,7 @@ contract BasePriceTracker is ChainlinkClient, KeeperCompatibleInterface {
      * @param _asset the asset name.
      * @return _ the price of the asset.
      */
+     //slither-disable-next-line naming-convention
     function getAssetPrice(string memory _asset) external view returns (uint256) {
         require(bytes(_asset).length > 0, "The asset name cannot be empty");
         require(assetToPrice[_asset].exists, "The asset must already be registered in the contract");
@@ -293,6 +299,7 @@ contract BasePriceTracker is ChainlinkClient, KeeperCompatibleInterface {
      * @notice Update the hub address.
      * @param _hubAddress the new hub address.
      */
+     //slither-disable-next-line naming-convention
     function setHubAddress(address _hubAddress) external onlyOwner {
         require(_hubAddress != address(0), "The hub address cannot be null");
 
@@ -304,6 +311,7 @@ contract BasePriceTracker is ChainlinkClient, KeeperCompatibleInterface {
      * @notice Update the aggregator address and the USDC/USD price feed.
      * @param _aggregatorAddress the new USDC/USD aggregator address.
      */
+     //slither-disable-next-line naming-convention
     function setAggregatorAddress(address _aggregatorAddress) external onlyOwner {
         require(_aggregatorAddress != address(0), "The aggregator address cannot be null");
 
@@ -316,6 +324,7 @@ contract BasePriceTracker is ChainlinkClient, KeeperCompatibleInterface {
      * @notice Update the keepers registry address.
      * @param _keepersRegistryAddress the new keepers registry address.
      */
+     //slither-disable-next-line naming-convention
     function setKeepersRegistryAddress(address _keepersRegistryAddress) external onlyOwner {
         require(_keepersRegistryAddress != address(0), "The keepers registry address cannot be null");
 
@@ -327,6 +336,7 @@ contract BasePriceTracker is ChainlinkClient, KeeperCompatibleInterface {
      * @notice Update the oracle address.
      * @param _oracleAddress the new chainlink node operator address.
      */
+     //slither-disable-next-line naming-convention
     function setOracleAddress(address _oracleAddress) external onlyOwner {
         require(_oracleAddress != address(0), "The oracle address cannot be null");
 
@@ -338,6 +348,7 @@ contract BasePriceTracker is ChainlinkClient, KeeperCompatibleInterface {
      * @notice Update the job ID.
      * @param _jobId the new job ID.
      */
+     //slither-disable-next-line naming-convention
     function setJobId(bytes32 _jobId) external onlyOwner {
         require(_jobId.length > 0, "The job ID cannot be empty");
 
@@ -349,6 +360,7 @@ contract BasePriceTracker is ChainlinkClient, KeeperCompatibleInterface {
      * @notice Update the alpha vantage api key.
      * @param _apiKey the new api key.
      */
+     //slither-disable-next-line naming-convention
     function setApiKey(string memory _apiKey) external onlyOwner {
         require(bytes(_apiKey).length > 0, "The API key cannot be empty");
 
